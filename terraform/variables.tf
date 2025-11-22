9bd4b6871923bd608396b152cd86dbbe4f124be1
suganyanagaraj16-commits <suganya.nagaraj16@gmail.com>
2025-11-22 15:00:23 +0530

    Add GCP project ID and region variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,19 @@
-variable "env"
-{
-  default =" dev"
+variable "gcp_project_id" {
+  description = "The GCP project ID."
+  type        = string
 }
-variable "public_subnet_a_cidr"
-{
-  default =" 10.10.1.0/24"
+
+variable "gcp_region" {
+  description = "The GCP region to deploy resources in."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (e.g., dev, prod)."
+  type        = string
+}
+
+variable "subnet_cidr_range" {
+  description = "The CIDR range for the subnetwork."
+  type        = string
 }
